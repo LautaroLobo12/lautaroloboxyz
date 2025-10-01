630c42a3269eed68aee48476718c62a821501f8c
Lautaro Lobo <contact@lautarolobo.xyz>
2025-10-01 17:12:51 -0300

    fixed rss feed generation

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -7,10 +7,11 @@ export async function GET(context) {
     title: 'Lautaro Lobo Personal Blog',
     description: 'My Personal Blog build with Astro',
     site: context.site,
+    trailingSlash: false,
     items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.date,
-      link: `/blog/${post.slug}/`,
+      link: `/blog/${post.id}/`,
     })),
   });
 }
